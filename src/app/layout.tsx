549de24ad4a48d7d4eed4c67aa643cf02e372a54
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-10-29 22:49:26 +1100

    got basic sidebar functional

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar"
 import AppSidebar from "../components/app-sidebar.tsx"
 
 const geistSans = Geist({
