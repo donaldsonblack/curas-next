d6826ffbb05b32afd49282584637b3612d3101b2
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-10-29 23:32:26 +1100

    added simple js array to map for header for departments

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,35 +1,45 @@
-import { 
-  Sidebar, 
-  SidebarContent, 
+import {
+  Sidebar,
+  SidebarContent,
   SidebarGroup,
-  SidebarMenu, 
-  SidebarMenuButton, 
-  SidebarMenuItem 
-} from "@/components/ui/sidebar"
-import { 
-  DropdownMenu, 
-  DropdownMenuContent, 
-  DropdownMenuItem, 
-  DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu"
-import { ChevronDown, Building2 } from "lucide-react"
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, Building2 } from "lucide-react";
+
+const wards = [
+	{ id: 1, name: "Surgery"},
+	{ id: 2, name: "Emergency"},
+	{ id: 3, name: "Cardiology"},
+]
 
 export function AppSidebar() {
   return (
     <Sidebar variant="floating" collapsible="icon">
+      {/*Scrollable content area*/}
       <SidebarContent>
+			{/*Logical grouping with spacing*/}
         <SidebarGroup>
+				{/*List container (ul)*/}
           <SidebarMenu>
+					{/*Individual list item (li) */}
             <SidebarMenuItem>
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                   <SidebarMenuButton>
-                    <Building2 />  {/* Add this icon */}
+                    <Building2 />
                     <span>Select Workspace</span>
                     <ChevronDown className="ml-auto" />
                   </SidebarMenuButton>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent 
+                <DropdownMenuContent
                   className="w-[--radix-popper-anchor-width]"
                   align="start"
                   side="bottom"
@@ -47,5 +57,5 @@ export function AppSidebar() {
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
-  )
+  );
 }
