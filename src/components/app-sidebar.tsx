04408c2cb26598524564fbf63b3cc056ef01d107
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-10-29 23:25:37 +1100

    added sidebar header with workspace for placeholder

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,10 +1,51 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar"
-
+import { 
+  Sidebar, 
+  SidebarContent, 
+  SidebarGroup,
+  SidebarMenu, 
+  SidebarMenuButton, 
+  SidebarMenuItem 
+} from "@/components/ui/sidebar"
+import { 
+  DropdownMenu, 
+  DropdownMenuContent, 
+  DropdownMenuItem, 
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import { ChevronDown, Building2 } from "lucide-react"
 
 export function AppSidebar() {
   return (
     <Sidebar variant="floating" collapsible="icon">
-      <SidebarContent />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <Building2 />  {/* Add this icon */}
+                    <span>Select Workspace</span>
+                    <ChevronDown className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent 
+                  className="w-[--radix-popper-anchor-width]"
+                  align="start"
+                  side="bottom"
+                >
+                  <DropdownMenuItem>
+                    <span>Acme Inc</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <span>Acme Corp.</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
   )
 }
